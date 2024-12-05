f79e5a10da50f6a2b9fcdd4798086e5ec2f168a8
carce <carcelesfranck@gmail.com>
2024-12-06 00:56:03 +0100

    retro css

--- src/vue/pageRetro/vueRetro.php
+++ src/vue/pageRetro/vueRetro.php
@@ -1,6 +1,3 @@
-<h1>Afficher un GIF</h1>
-<!-- Insérer une image GIF via PHP -->
-<img src='../../../ressources/water.gif' alt='Un GIF chargé dynamiquement'>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
