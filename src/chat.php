02c05ebee8a43068ee6c4217c246433265b4d5a7
Andriamihaja <manu.andriamihaja-razafimanjato@etu.umontpellier.fr>
2024-12-06 02:29:57 +0100

    Ajout des easter eggs

--- src/chat.php
+++ src/chat.php
@@ -2,18 +2,44 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userMessage = $_POST['message'] ?? '';
 
+
     // Si le message n'est pas vide
     if (!empty($userMessage)) {
-        $response = getHuggingFaceResponse($userMessage);
-
-        if (isset($response['choices'][0]['message']['content'])) {
-            // Retourner la réponse du chatbot
-            echo $response['choices'][0]['message']['content'];
-        } else {
-            // Si aucune réponse n'est reçue ou erreur dans l'API
-            echo 'Une erreur est survenue avec l\'API Hugging Face.';
+        if ($userMessage === 'Tu te bats comme une vache !') {
+            echo 'C\'est normal, tu as l\'odorat d\'un porc-épic !';
         }
-    } else {
+        if ($userMessage === 'Quel est le secret de Monkey Island ?') {
+            echo 'C\'est une recette de grog ultra-secrète... mais tu dois promettre de ne jamais le dire à LeChuck !';
+        }
+        if ($userMessage === 'Grog') {
+            echo 'Ah, le Grog ! Un mélange savoureux de kérosène, propylène glycol, huile pour batterie, et d\'autres ingrédients impossibles à prononcer. À consommer avec modération (ou pas) !';
+        }
+        if ($userMessage === 'Qui es-tu ?" ou "Quel est ton nom ?') {
+            echo 'Je suis Guybrush Threepwood, un puissant pirate !';
+        }
+        if ($userMessage === 'ouvrir le coffre') {
+            echo 'Le coffre est verrouillé. La clé ? Elle est probablement coincée dans une grotte hantée, derrière un singe à trois têtes.';
+        }
+        if ($userMessage === 'danse') {
+            echo 'Tu ne peux pas prendre la clé, elle est derrière le sing';
+        }
+        else {
+            $response = getHuggingFaceResponse($userMessage);
+            echo $response['choices'][0]['message']['content'];
+        }
+    }
+//        $response = getHuggingFaceResponse($userMessage);
+//        if (isset($response['choices'][0]['message']['content'])) {
+//            // Retourner la réponse du chatbot
+//            echo $response['choices'][0]['message']['content'];
+//        }
+//        else {
+//            // Si aucune réponse n'est reçue ou erreur dans l'API
+//            echo 'Une erreur est survenue avec l\'API Hugging Face.';
+//        }
+//    }
+
+    else {
         echo 'Message vide.';
     }
 }
