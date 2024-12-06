fff238253a134a20e9c0b5588ca080cda35d9c41
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-12-06 06:15:00 +0100

    Défi Logo Lyreco

--- src/vue/vueGenerale.php
+++ src/vue/vueGenerale.php
@@ -12,15 +12,22 @@ echo $cheminCSS;
          * @var string $titre
          */
         echo $titre; ?></title>
+    <link rel="stylesheet" href="../ressources/css/logo.css">
 </head>
 <body>
 <main>
+    <div class="ball"></div>
+    <div class="infobulle">
+        <div class="goutte"></div>
+        <p class="info"></p>
+    </div>
     <?php
     /**
      * @var string $cheminCorpsVue
      */
     require __DIR__ . "/{$cheminCorpsVue}";
     ?>
+    <script src="../ressources/js/scriptLogo.js"></script>
 </main>
 <footer>
     <p>Site de NeuilleVille-sur-Seine</p>
